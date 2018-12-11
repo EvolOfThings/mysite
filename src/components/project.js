60755d26dc6d2311c0ad78e7e3b28beaa5ebd1b8
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-12-11 22:49:27 +0530

    project component

---
+++ src/components/project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+        return (
+            <div className="project-box">
+            <p>Heyy!!</p>
+            </div>
+        )
+    }
+}
+
+export default Project;
